a3816b7b9bcc1dbf7323de7605512bf320ecf9f3
arfycat <arfycat@users.noreply.github.com>
2022-05-04 13:22:15 -0400

    Working packaging for Debian.

--- linux-debian/make.sh
+++ linux-debian/make.sh
@@ -3,13 +3,15 @@
 # Based on: https://betterprogramming.pub/how-to-create-a-basic-debian-package-927be001ad80
 #
 
+umask 022
+
 DIR="$(dirname "$(realpath "$0")")"
 STAGEDIR="${DIR}/work"
 VERSION="$(date "+%Y%m%d-%H%M%S")"
 PKGDIR="${STAGEDIR}/arfycat-utils_${VERSION}_all"
 
 if [ -d "${STAGEDIR}" ]; then
-  rm -rf -- "${STAGEDIR}" || exit 1
+  sudo rm -rf -- "${STAGEDIR}" || exit 1
 fi
 
 if [ ! -d "${STAGEDIR}" ]; then
@@ -34,9 +36,20 @@ Description: A collection of utilities used by Arfycat hosts.
 Depends: bash, curl, util-linux
 EOF
 
-mkdir -p "${PKGDIR}/usr/local/bin" || exit 1
-mkdir -p "${PKGDIR}/usr/local/share/arfycat" || exit 1
-cp "${DIR}/../bash/bashutils.sh" "${PKGDIR}/usr/local/share/arfycat/" || exit 1
-cp "${DIR}/../bash/hc" "${PKGDIR}/usr/local/bin/" || exit 1
+#
+# DEBIAN/conffiles
+#
+cat > "${PKGDIR}/DEBIAN/conffiles" << EOF || exit 1
+/etc/hc.conf
+EOF
+
+mkdir -p "${PKGDIR}/etc" || exit 1
+mkdir -p "${PKGDIR}/usr/bin" || exit 1
+mkdir -p "${PKGDIR}/usr/share/arfycat" || exit 1
+cp "${DIR}/../bash/bashutils.sh" "${PKGDIR}/usr/share/arfycat/" || exit 1
+cp "${DIR}/../bash/hc" "${PKGDIR}/usr/bin/" || exit 1
+cp "${DIR}/../bash/hc.conf" "${PKGDIR}/etc/"
+sudo chown -R root:root "${PKGDIR}" || exit 1
+sudo chmod -R u+Xrw,g+Xr-w,o+Xr-w "${PKGDIR}" || exit 1
 
 dpkg-deb --build "${PKGDIR}" || exit 4
