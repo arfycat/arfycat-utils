12447858940abf60ec74f5e433b2843ec561bda6
arfycat <arfycat@users.noreply.github.com>
2022-05-09 09:28:57 -0400

    Use env to find bash.

--- bash/rclone-b2.sh
+++ bash/rclone-b2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if ! PATH="${PATH}:/usr/local/share/arfycat:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
 umask 077
 
