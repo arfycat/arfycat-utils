896f9eddbc19a48942fd85c40f1079e41d2d3758
arfycat <arfycat@users.noreply.github.com>
2022-06-07 08:44:01 -0400

    Changes to make more useful.

--- bash/rsync.sh
+++ bash/rsync.sh
@@ -1,50 +1,76 @@
 #!/usr/bin/env bash
-if ! PATH="${PATH}:/usr/local/share/arfycat:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
-set -o pipefail
-umask 077
+{
+  if ! PATH="${PATH}:/usr/local/share/arfycat:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
+  set -o pipefail
 
-if [ ! -d "${HOME}" ]; then
-  fail 1 "Home directory does not exist: ${HOME}."
-fi
+  # Script must be sourced.
+  if [[ "$BASH_SOURCE" == "$0" ]]; then fail 1 "$0 must be sourced."; fi
 
-SCRIPT="$(basename $0)"; [[ $? -ne 0 ]] && fail 1 "Failed to get basename of script: $0"
-RSYNC_REMOTE_HOST="$(ssh -G "${SCRIPT}" | awk '/^hostname / { print $2 }')"; [[ $? -ne 0 ]] && fail 1 "Failed to determine remote hostname from SSH config."
-RSYNC="$(which rsync)"; [[ $? -ne 0 ]] && fail 1 "Failed to locate rsync."
-RSYNC_EXCLUDE_FILE="$(realpath $0)-exclude.txt"
-RSYNC_KEY="$(ssh -G "${SCRIPT}" | awk '/^identityfile / { print $2 }')"; [[ $? -ne 0 ]] && fail 1 "Failed to determine identity file from SSH config."
-OPTS="--exclude-from=${RSYNC_EXCLUDE_FILE} --chmod=u+Xrw,g+Xr,g-w,o-Xrw --delete --delete-excluded -azltRi --safe-links --timeout=3600"
-SSH_CMD="sshpass -P '${RSYNC_KEY}' -f ${RSYNC_KEY}.txt ssh -T"
+  SCRIPT="$(realpath "$0")"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed: realpath $0"
+  DIR="$(dirname "${SCRIPT}")"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed: dirname $SCRIPT"
+  BASENAME="$(basename $0)"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed: basename $0"
+  NAME="${BASENAME%.*}"
 
-if [ ! -r "${RSYNC_EXCLUDE_FILE}" ]; then
-  fail 1 "Cannot read exclude file: ${RSYNC_EXCLUDE_FILE}."
-fi
+  RSYNC_REMOTE_HOST="$(ssh -G "${NAME}" | awk '/^hostname / { print $2 }')"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed to determine remote hostname from SSH config."
+  RSYNC="$(which rsync)"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed to locate rsync."
+  RSYNC_EXCLUDE_FILE="${DIR}/${NAME}-exclude.txt"
+  RSYNC_FROM_FILE="${DIR}/${NAME}-from.txt"
+  RSYNC_KEY="$(ssh -G "${NAME}" | awk '/^identityfile / { print $2 }')"; [[ $? -ne 0 ]] && fail 1 "Failed to determine identity file from SSH config."
+  OPTS="--chmod=D750,F640 --delete --delete-excluded --delete-after -rltzRi --open-noatime --safe-links --timeout=3600"
+  export RSYNC_RSH="sshpass -P '${RSYNC_KEY}' -f ${RSYNC_KEY}.txt ssh -T"
 
-if [ "${RSYNC_REMOTE_HOST}" == "${SCRIPT}" ]; then
-  fail 1 "Failed to lookup hostname in SSH config."
-fi
-
-lock 3600 "${RSYNC_REMOTE_HOST}"
-
-cmd() {
-  if ! cd ${1}; then
-    return 1
+  if [[ -r "${RSYNC_EXCLUDE_FILE}" ]]; then
+    OPTS+=" --exclude-from=${RSYNC_EXCLUDE_FILE}"
   fi
 
-  shift
-  "$@"
-  RET=$?
-  return ${RET}
-}
-
-sync() {
-  if [[ $# -ne 2 ]]; then
-    echo "Incorrect arguments: sync <source> <destination>"
-    return 1
+  if [[ -r "${RSYNC_FROM_FILE}" ]]; then
+    OPTS+=" --files-from=${RSYNC_FROM_FILE}"
   fi
 
-  SRC="$1"
-  DST="$2"
+  if [[ "${RSYNC_REMOTE_HOST}" == "${NAME}" ]]; then
+    fail 1 "Failed to lookup hostname in SSH config."
+  fi
 
-  cmd /tmp "${RSYNC}" ${OPTS} --rsh "${SSH_CMD}" "${SRC}" "${DST}"
-  return $?
+  lock 0 "${NAME}"
+
+  cmd() {
+    if [[ $# -lt 2 ]]; then
+      echo "Incorrect arguments: cmd <working diretory> <command> [arg1] ... [argN]"
+      return 1
+    fi
+
+    cd "$1" || return $?
+    shift
+
+    if [[ -v DEBUG ]]; then echo "$@"; fi
+
+    "$@"
+    return $?
+  }
+
+  sync() {
+    if [[ $# -lt 2 ]]; then
+      echo "Incorrect arguments: sync <source> <destination> [working directory] [rsync args]"
+      return 1
+    fi
+
+    SRC="$1"; shift
+    DST="$1"; shift
+    
+    if [[ $# -gt 0 ]]; then
+      local DIR="$1"; shift
+    else
+      local DIR="/tmp"
+    fi
+    
+    if [[ ! -d "${DIR}" ]]; then
+      echo "Directory does not exist: ${DIR}"
+      return 1
+    fi
+
+    cmd "${DIR}" "${RSYNC}" ${OPTS} "$@" "${SRC}" "${DST}"
+    return $?
+  }
+  
+  return 0
 }
