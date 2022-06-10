fca1d3195aef30ce59c69c98ac3b5ed3c9b8d313
arfycat <arfycat@users.noreply.github.com>
2022-06-10 14:16:24 -0400

    Use cmd from bashutils.sh.

--- bash/rsync.sh
+++ bash/rsync.sh
@@ -8,7 +8,7 @@
 
   SCRIPT="$(realpath "$0")"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed: realpath $0"
   DIR="$(dirname "${SCRIPT}")"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed: dirname $SCRIPT"
-  BASENAME="$(basename $0)"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed: basename $0"
+  BASENAME="$(basename $0)"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "ut: basename $0"
   NAME="${BASENAME%.*}"
 
   RSYNC_REMOTE_HOST="$(ssh -G "${NAME}" | awk '/^hostname / { print $2 }')"; _R=$?; [[ $_R -ne 0 ]] && fail $_R "Failed to determine remote hostname from SSH config."
@@ -16,7 +16,7 @@
   RSYNC_EXCLUDE_FILE="${DIR}/${NAME}-exclude.txt"
   RSYNC_FROM_FILE="${DIR}/${NAME}-from.txt"
   RSYNC_KEY="$(ssh -G "${NAME}" | awk '/^identityfile / { print $2 }')"; [[ $? -ne 0 ]] && fail 1 "Failed to determine identity file from SSH config."
-  OPTS="--chmod=D750,F640 --delete --delete-excluded --delete-after -rltzRi --open-noatime --safe-links --timeout=3600"
+  OPTS="--chmod=D750,F640 --delete --delete-excluded --delete-after -rltzRi --open-noatime --safe-links --timeout=1200 --outbuf=L"
   export RSYNC_RSH="sshpass -P '${RSYNC_KEY}' -f ${RSYNC_KEY}.txt ssh -T"
 
   if [[ -r "${RSYNC_EXCLUDE_FILE}" ]]; then
@@ -32,21 +32,6 @@
   fi
 
   lock 0 "${NAME}"
-
-  cmd() {
-    if [[ $# -lt 2 ]]; then
-      echo "Incorrect arguments: cmd <working diretory> <command> [arg1] ... [argN]"
-      return 1
-    fi
-
-    cd "$1" || return $?
-    shift
-
-    if [[ -v DEBUG ]]; then echo "$@"; fi
-
-    "$@"
-    return $?
-  }
 
   sync() {
     if [[ $# -lt 2 ]]; then
@@ -68,7 +53,7 @@
       return 1
     fi
 
-    cmd "${DIR}" "${RSYNC}" ${OPTS} "$@" "${SRC}" "${DST}"
+    cmd "${RSYNC}" ${OPTS} "$@" "${SRC}" "${DST}" 2>&1 | { grep --line-buffered -v "Pseudo-terminal will not be allocated because stdin is not a terminal."; true; }
     return $?
   }
   
