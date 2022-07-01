968907b5bc8c9f6e43378d722fded8a1bd1ed032
arfycat <arfycat@users.noreply.github.com>
2022-07-01 10:15:03 -0400

    Support different password types and no password.

--- bash/cron-status.sh
+++ bash/cron-status.sh
@@ -1,16 +1,56 @@
 #!/usr/bin/env bash
-if ! PATH="${PATH}:/usr/local/share/arfycat:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
-HOST="$(basename "$0")"
-ID="$(get_home)/.ssh/id_ed25519"
+{
+  if ! PATH="${PATH}:/usr/local/share/arfycat:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
+  HOST="$(basename "$0")"; HOST="${HOST%.*}"
+  PW_FILE="${HOME}/.ssh/${HOST}.txt"
 
-TMP_LOG=; get_tmp_file TMP_LOG
-$(dirname "$(realpath "$0")")/status.sh > ${TMP_LOG} 2>&1
-RET=$?
-cat "${TMP_LOG}"
+  cd /tmp || fail $? "Failed to change directory."
 
-SFTP_LOG=; get_tmp_file SFTP_LOG
-if ! echo "PUT ${TMP_LOG} status.txt" | timeout 15s sshpass -P "passphrase" -f "${ID}.txt" sftp -i "${ID}" ${HOST} >> ${SFTP_LOG} 2>&1; then
-  cat "${SFTP_LOG}"
-fi
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -pp)
+        PASSPHRASE=
+        shift
+        ;;
+      -pw)
+        PASSWORD=
+        shift
+        ;;
+      -pi)
+        PIN=
+        shift
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
 
-exit ${RET}
+  TMP_LOG=; get_tmp_file TMP_LOG
+  $(dirname "$(realpath "$0")")/status.sh > ${TMP_LOG} 2>&1
+  RET=$?
+  cat "${TMP_LOG}"
+
+  SFTP_LOG=; get_tmp_file SFTP_LOG
+  if [[ -r "${PW_FILE}" ]]; then
+    if [[ -v PIN ]]; then
+      echo "PUT ${TMP_LOG} status.txt" | timeout 15s sshpass -P "PIN" -f "${PW_FILE}" sftp ${HOST} >> ${SFTP_LOG} 2>&1
+      _R=$?
+    elif [[ -v PASSWORD ]]; then
+      echo "PUT ${TMP_LOG} status.txt" | timeout 15s sshpass -f "${PW_FILE}" sftp ${HOST} >> ${SFTP_LOG} 2>&1
+      _R=$?
+    else
+      echo "PUT ${TMP_LOG} status.txt" | timeout 15s sshpass -P "passphrase" -f "${PW_FILE}" sftp ${HOST} >> ${SFTP_LOG} 2>&1
+      _R=$?
+    fi
+  else
+    echo "PUT ${TMP_LOG} status.txt" | timeout 15s sftp ${HOST} >> ${SFTP_LOG} 2>&1
+    _R=$?
+  fi
+
+  if [[ $_R -ne 0 ]]; then
+    cat "${SFTP_LOG}"
+  fi
+
+  exit ${RET}
+}
