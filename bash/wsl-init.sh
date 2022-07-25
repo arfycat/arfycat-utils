7f11d3171251a11d49b9137f75f6958005ef8673
arfycat <arfycat@users.noreply.github.com>
2022-07-25 15:48:37 -0400

    Check for init script before running service.

--- bash/wsl-init.sh
+++ bash/wsl-init.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bash
-if ! PATH="${PATH}:/usr/local/share/arfycat:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
-umask 077
+#!/bin/bash
+{
+  if ! PATH="${PATH}:/usr/local/share/arfycat:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
+  umask 077
 
-lock
-log
+  lock
+  log
 
-echo "$(date): $0 $@"
-cd /tmp || exit $?
+  echo "$(date): $0 $@"
+  cd /tmp || exit $?
 
-while :; do
-  date
-  service rsyslog status || service rsyslog restart || exit $?
-  service cron status || service cron restart || exit $?
-  sleep 360
-done
+  while :; do
+    date
+    if [[ -f /etc/init.d/rsyslog ]]; then service rsyslog status || service rsyslog restart || exit $?; fi
+    if [[ -f /etc/init.d/cron ]]; then service cron status || service cron restart || exit $?; fi
+    sleep 600
+  done
+  exit 0
+}
