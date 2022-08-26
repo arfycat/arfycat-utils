3fdf4210af2bcd002ddb94e0cbd350b47d61bbbd
arfycat <arfycat@users.noreply.github.com>
2022-08-26 07:38:51 -0400

    Added workaround for WSL Ubuntu to start cron and rsyslogd.

--- bash/wsl-init.sh
+++ bash/wsl-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {
-  if ! PATH="${PATH}:/usr/local/share/arfycat:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
+  if ! PATH="${PATH}:/usr/share/arfycat" source bashutils.sh; then echo Failed to source arfycat/bashutils.sh; exit 255; fi
   umask 077
 
   user root "$@"
@@ -15,11 +15,15 @@
     date
     if [[ -f /etc/init.d/rsyslog ]]; then
       service rsyslog status || service rsyslog restart || exit $?
-    elif [[ -x /root/rsyslogd.sh ]]; then
-      /root/rsyslogd.sh || exit $?
+    else
+      /usr/share/arfycat/rsyslogd.sh || exit $?
     fi
-    
-    if [[ -f /etc/init.d/cron ]]; then service cron status || service cron restart || exit $?; fi
+
+    if [[ -f /etc/init.d/cron ]]; then
+      service cron status || service cron restart || exit $?
+    else
+      /usr/share/arfycat/cron.sh || exit $?
+    fi
     sleep 300
   done
   exit 0
