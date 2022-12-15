4008c9a67b20c19dfabd2bb69be543604ced00f0
arfycat <arfycat@users.noreply.github.com>
2022-12-15 10:50:12 -0500

    Added more options and recursive DNS lookup via different server.

--- bash/nsupdate.sh
+++ bash/nsupdate.sh
@@ -6,6 +6,35 @@
 
   lock
 
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -4)
+        IPV4=
+        shift
+        ;;
+      -6)
+        IPV6=
+        shift
+        ;;
+      debug)
+        DEBUG=
+        shift
+        ;;
+      force)
+        FORCE=
+        shift
+      ;;
+      *)
+        break
+        ;;
+    esac
+  done
+
+  if [[ ! -v IPV4 && ! -v IPV6 ]]; then
+    IPV4=
+    IPV6=
+  fi
+
   . $HOME/nsupdate.env || fail $? "Failed to source nsupdate.env file."
 
   IPV4_RE="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
@@ -18,23 +47,72 @@
     NSUPDATE="$(which nsupdate)"
   fi
 
+  _query() {
+    local TYPE="$1"
+    local HOST="$2"
+    local -n VAR=$3
+
+    if [[ -v DIG ]]; then
+      VAR="$(dig +short -r -t $TYPE -p $RPORT @$RSERVER $HOST)" || true
+    elif [[ -v DRILL ]]; then
+      # The FreeBSD drill doesn't have the short option yet.
+      VAR="$(drill -p $RPORT $HOST @$RSERVER $TYPE | grep -Ev '^;;|^$|SOA' | cut -w -f5)"
+    else
+      exit 1 "No supported DNS client."
+    fi
+
+    if [[ -v DEBUG ]]; then
+      echo "_query $TYPE $HOST -> $VAR"
+    fi
+    return 0
+  }
+
+  _query_nr() {
+    local TYPE="$1"
+    local HOST="$2"
+    local -n VAR=$3
+
+    if [[ -v DIG ]]; then
+      VAR="$(dig +norecurse +short -r -t $TYPE -p $PORT @$SERVER $HOST)" || true
+    elif [[ -v DRILL ]]; then
+      # The FreeBSD drill doesn't have the short option yet.
+      VAR="$(drill -o rd -p $PORT $HOST @$SERVER $TYPE | grep -Ev '^;;|^$|SOA' | cut -w -f5)"
+    else
+      exit 1 "No supported DNS client."
+    fi
+
+    if [[ -v DEBUG ]]; then
+      echo "_query_nr $TYPE $HOST -> $VAR"
+    fi
+    return 0
+  }
+
+  _host_from_url() {
+    local URL="$1"
+    local -n VAR=$2
+
+    if [[ $URL =~ ^.+://([^/?]+) ]]; then
+      VAR="${BASH_REMATCH[1]}"
+      if [[ -v DEBUG ]]; then
+        echo "_host_from_url $URL -> $VAR"
+      fi
+      return 0
+    else
+      echo "Failed to obtain host from URL: ${URL}"
+      return 1
+    fi
+  }
+ 
   _nsupdate() {
     local TYPE="$1"
     local DATA="$2"
     local HOST="$3"
     local ZONE="$4"
 
-    if [[ -v DIG ]]; then
-      local QUERY="$(dig +norecurse +short -r -t $TYPE -p $PORT @$SERVER $HOST)" || true
-    elif [[ -v DRILL ]]; then
-      # The FreeBSD drill doesn't have the short option yet.
-      local QUERY="$(drill -o rd -p $PORT $HOST @$SERVER $TYPE | grep -Ev '^;;|^$|SOA' | cut -w -f5)"
-    else
-      exit 1 "No supported DNS client."
-    fi
-
-    if [[ $QUERY == "" || $QUERY != $DATA ]]; then
-      echo "$HOST $TYPE $DATA"
+    local QUERY=; _query_nr "$TYPE" "$HOST" QUERY
+ 
+    if [[ -v FORCE || $QUERY == "" || $QUERY != $DATA ]]; then
+      echo "Updating $HOST $TYPE $DATA"
       ${NSUPDATE} -k $HOME/nsupdate.key <<EOF
 server $SERVER $PORT
 zone $ZONE
@@ -47,28 +125,50 @@ EOF
   }
 
   if [[ -v HOST_E && $HOST_E != "" ]]; then
-    EIPV4="$(curl -s4 "${IP_URL_E}")" || true
-    EIPV6="$(curl -s6 "${IP_URL_E}")" || true
+    IP_URL_E_HOST=; _host_from_url $IP_URL_E IP_URL_E_HOST
 
-    if [[ $EIPV4 =~ $IPV4_RE ]]; then
-      _nsupdate A $EIPV4 $HOST_E $HOST_E
+    if [[ -v IPV4 ]]; then
+      IP_URL_E_IP4=; _query A "$IP_URL_E_HOST" IP_URL_E_IP4
+      if [[ $IP_URL_E_IP4 =~ $IPV4_RE ]]; then
+        EIPV4="$(curl --dns-servers $RSERVER:$RPORT -s4 --resolve "$IP_URL_E_HOST:443:$IP_URL_E_IP4" "$IP_URL_E")" || true
+        if [[ $EIPV4 =~ $IPV4_RE ]]; then
+          _nsupdate A $EIPV4 $HOST_E $HOST_E
+        fi
+      fi
     fi
 
-    if [[ $EIPV6 =~ $IPV6_RE ]]; then
-      _nsupdate AAAA $EIPV6 $HOST_E $HOST_E
+    if [[ -v IPV6 ]]; then
+      IP_URL_E_IP6=; _query AAAA "$IP_URL_E_HOST" IP_URL_E_IP6
+      if [[ $IP_URL_E_IP6 =~ $IPV6_RE ]]; then
+        EIPV6="$(curl --dns-servers $RSERVER:$RPORT -s6 --resolve "$IP_URL_E_HOST:443:$IP_URL_E_IP6" "$IP_URL_E")" || true
+        if [[ $EIPV6 =~ $IPV6_RE ]]; then
+          _nsupdate AAAA $EIPV6 $HOST_E $HOST_E
+        fi
+      fi
     fi
   fi
 
   if [[ -v HOST_I && $HOST_I != "" ]]; then
-    IIPV4="$(curl -s4 "${IP_URL_I}")" || true
-    IIPV6="$(curl -s6 "${IP_URL_I}")" || true
+    IP_URL_I_HOST=; _host_from_url $IP_URL_I IP_URL_I_HOST
 
-    if [[ $IIPV4 =~ $IPV4_RE ]]; then
-      _nsupdate A $IIPV4 $HOST_I $HOST_I
+    if [[ -v IPV4 ]]; then
+      IP_URL_I_IP4=; _query A "$IP_URL_I_HOST" IP_URL_I_IP4
+      if [[ $IP_URL_I_IP4 =~ $IPV4_RE ]]; then
+        IIPV4="$(curl --dns-servers $RSERVER:$RPORT -s4 --resolve "$IP_URL_I_HOST:443:$IP_URL_I_IP4" "$IP_URL_I")" || true
+        if [[ $IIPV4 =~ $IPV4_RE ]]; then
+          _nsupdate A $IIPV4 $HOST_I $HOST_I
+        fi
+      fi
     fi
 
-    if [[ $IIPV6 =~ $IPV6_RE ]]; then
-      _nsupdate AAAA $IIPV6 $HOST_I $HOST_I
+    if [[ -v IPV6 ]]; then
+      IP_URL_I_IP6=; _query AAAA "$IP_URL_I_HOST" IP_URL_I_IP6
+      if [[ $IP_URL_I_IP6 =~ $IPV6_RE ]]; then
+        IIPV6="$(curl --dns-servers $RSERVER:$RPORT -s6 --resolve "$IP_URL_I_HOST:443:$IP_URL_I_IP6" "$IP_URL_I")" || true
+        if [[ $IIPV6 =~ $IPV6_RE ]]; then
+          _nsupdate AAAA $IIPV6 $HOST_I $HOST_I
+        fi
+      fi
     fi
   fi
 
